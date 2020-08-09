4e2b068edd1da5f192d917278803a24d28e2a708
Gabriel Ostrolucký <gabriel.ostrolucky@gmail.com>
2020-08-09 17:23:40 +0200

    Fix "Only Stripe Connect platforms can work with other accounts." errors

--- src/StripePsr18Client.php
+++ src/StripePsr18Client.php
@@ -6,6 +6,7 @@ namespace Psr18Adapter\Stripe;
 
 use Psr\Http\Client\ClientInterface as PsrClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriFactoryInterface;
 use Stripe\HttpClient\ClientInterface;
 
@@ -41,17 +42,34 @@ class StripePsr18Client implements ClientInterface
      */
     public function request($method, $absUrl, $headers, $params, $hasFile): array
     {
-        $request = $this->requestFactory->createRequest(
-            $method,
-            $this->uriFactory->createUri($absUrl)->withQuery(http_build_query($params, '', '&', PHP_QUERY_RFC3986))
+        $response = $this->client->sendRequest(
+            $this->setNormalizedHeaders(
+                $this->requestFactory->createRequest(
+                    $method,
+                    $this->uriFactory->createUri($absUrl)->withQuery(http_build_query($params))
+                ),
+                $headers
+            )
         );
 
-        foreach ($headers as $header) {
-            $request = $request->withHeader($key = strstr($header, ':', true), substr($header, strlen($key) + 2));
+        return [$response->getBody()->__toString(), $response->getStatusCode(), $response->getHeaders()];
+    }
+
+    /**
+     * @param list<string> $rawHeaders
+     */
+    private function setNormalizedHeaders(RequestInterface $request, array $rawHeaders): RequestInterface
+    {
+        foreach ($rawHeaders as $header) {
+            $key = strstr($header, ':', true);
+            $value = substr($header, strlen($key) + 2);
+            // These headers must not be sent with empty values, otherwise you get error responses such as "Only Stripe Connect platforms can work with other accounts."
+            if (in_array($key, ['Stripe-Account', 'Stripe-Version']) && !$value) {
+                continue;
+            }
+            $request = $request->withHeader($key, $value);
         }
 
-        $response = $this->client->sendRequest($request);
-
-        return [$response->getBody()->__toString(), $response->getStatusCode(), $response->getHeaders()];
+        return $request;
     }
 }
---
+++ tests/IntegrationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psr18Adapter\Stripe\Tests;
+
+use GuzzleHttp\Client;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use PHPUnit\Framework\TestCase;
+use Psr18Adapter\Stripe\StripePsr18Client;
+use Stripe\ApiRequestor;
+use Stripe\Collection;
+use Stripe\StripeClient;
+
+class IntegrationTest extends TestCase
+{
+    /** @var StripeClient */
+    private $stripeClient;
+    public function setUp(): void
+    {
+        if (empty($_SERVER['STRIPE_CLIENT_ID'])) {
+            self::markTestSkipped('STRIPE_CLIENT_ID environment variable needs to be set to run integration tests');
+        }
+
+        ApiRequestor::setHttpClient(new StripePsr18Client(new Client(), new Psr17Factory(), new Psr17Factory()));
+        $this->stripeClient = new StripeClient($_SERVER['STRIPE_CLIENT_ID']);
+    }
+
+    public function testGetAllCustomers()
+    {
+        self::assertInstanceOf(Collection::class, $this->stripeClient->customers->all(['limit' => 1]));
+    }
+}
